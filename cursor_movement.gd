ec22ed5a2433e722288c2183190f30bf7bd3f58e
koukhub <matejkoukola@gmail.com>
2026-09-15 17:11:39 +0200

    Add files via upload

---
+++ cursor_movement.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+
+
+const SPEED = 100.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("left", "right", "up", "down")
+	velocity = direction * SPEED
+	move_and_slide()
