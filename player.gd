b2fdf34beccdb7bd54eb459ced8d610904810a84
Matěj Koukola <matejkoukola@gmail.com>
2026-09-18 22:39:53 +0200

    break animation

--- player.gd
+++ player.gd
@@ -6,32 +6,36 @@ const SPEED = 250.0
 const max_tilt = deg_to_rad(10)
 const tilt_speed = 8.0
 
-@export var texture_idle: Texture2D
-@export var texture_left: Texture2D
-@export var texture_right: Texture2D
-
 signal moved_mouse(dir)
-
+var can_move = true
 func _physics_process(delta: float) -> void:
-	var target_tilt = 0.0
-	
-	if velocity.x < 0:
-		target_tilt = -max_tilt
-	elif velocity.x > 0:
-		target_tilt = max_tilt
-	var direction := Input.get_vector("left", "right", "up", "down")
-	velocity = direction * SPEED
-	sprite.rotation = lerp_angle(sprite.rotation,target_tilt,delta * tilt_speed)
-	move_and_slide()
-	var moved_by = get_real_velocity() * delta
-	moved_mouse.emit(moved_by)
-	
-	update_texture()
+	if can_move:
+		var target_tilt = 0.0
+		
+		if velocity.x < 0:
+			target_tilt = -max_tilt
+		elif velocity.x > 0:
+			target_tilt = max_tilt
+		var direction := Input.get_vector("left", "right", "up", "down")
+		velocity = direction * SPEED
+		$AnimatedSprite2D.rotation = lerp_angle($AnimatedSprite2D.rotation,target_tilt,delta * tilt_speed)
+		move_and_slide()
+		var moved_by = get_real_velocity() * delta
+		moved_mouse.emit(moved_by)
+		
+		update_texture()
 	
 func update_texture() -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		sprite.texture = texture_left
-	#elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-	#	sprite.texture = texture_right
-	else:
-		sprite.texture = texture_idle
+	if can_move:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			$AnimatedSprite2D.play("left_click")
+		#elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		#	sprite.texture = texture_right
+		else:
+			$AnimatedSprite2D.play("idle")
+
+
+func _on_floor_player_fell() -> void:
+	$AnimatedSprite2D.rotation = 0
+	$AnimatedSprite2D.play("fall")
+	can_move = false
--- scenes/floor.gd
+++ scenes/floor.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-
+signal player_fell()
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("you fell")
+		player_fell.emit()
