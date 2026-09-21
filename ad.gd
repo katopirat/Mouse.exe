6f9ef8f20634cf0d38143782660d9570cbab3a0b
koukhub <matejkoukola@gmail.com>
2026-09-21 20:36:59 +0200

    level 2 gameplay

--- ad.gd
+++ ad.gd
@@ -1,21 +1,25 @@
 extends Area2D
 var cursor_inside = false
-signal closed()
+
 
 func _on_body_entered(body: Node2D) -> void:
 	print(body.name)
 	if body.name == "Cursor":
 		cursor_inside = true
-		$AnimatedSprite2D.play("hovered")
  
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "Cursor":
 		cursor_inside = false
-		$AnimatedSprite2D.play("default")
 
 func _input(event):
 	
 	if cursor_inside and event.is_action_pressed("click"):
-		print("clicked x")
-		closed.emit()
+
+		get_tree().reload_current_scene()
 		
+
+
+
+
+func _on_x_button_ad_closed() -> void:
+	queue_free()
