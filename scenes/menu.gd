efd3a2d3c1bf3b546318a993f09f314b1146747d
VaMaT2 <vaclavczholub@gmail.com>
2026-09-21 17:15:32 +0200

    dfhgsfvb

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 
 func _on_play_pressed() -> void:
 	print("start")
-	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
