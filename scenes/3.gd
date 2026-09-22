00f9689b42cb59c8c38d3ab2ea8734b1f93861fd
VaMaT2 <vaclavczholub@gmail.com>
2026-09-22 18:52:06 +0200

    menutlacitko

--- scenes/3.gd
+++ scenes/3.gd
@@ -9,8 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_pressed() -> void:
-	print("1")
-	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
---
+++ scenes/menu_levels.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on__pressed() -> void:
+	
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
+
+func _on_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+
+func _on_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
+
+func _on_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_7_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_8_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_9_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_10_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_11_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_12_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_13_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_14_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_15_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_16_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_17_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_18_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
