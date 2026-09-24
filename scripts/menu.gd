c7e9d3ab6bbb1976dc806789dc4e5637eb1ead61
VaMaT2 <vaclavczholub@gmail.com>
2026-09-24 17:20:59 +0200

    menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -16,3 +16,7 @@ func _on_play_pressed() -> void:
 
 func _on_tut_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
